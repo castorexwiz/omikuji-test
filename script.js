a166f215e3eef84820c9a3ca05eea00d2f122b19
castorexwiz <perl.ex.wiz@gmail.com>
2025-05-13 16:14:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("draw").addEventListener("click", function () {
-  const fortunes = ["大吉", "中吉", "吉"];
+  const fortunes = ["大吉", "中吉", "吉", "メガ吉", "ギガ吉"];
   const result = fortunes[Math.floor(Math.random() * fortunes.length)];
   document.getElementById("result").textContent = `✨ ${result} ✨`;
 });
